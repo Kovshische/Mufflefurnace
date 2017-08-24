4b3e34a0726f0b9471d4e562600684fe77088aef
Kovshische <Kovshische@gmail.com>
2017-08-24 14:37:46 +0300

    Edit program  - add intent to go "home" (parent view)

--- app/src/main/java/com/example/android/mufflefurnace/ProgramEditActivity.java
+++ app/src/main/java/com/example/android/mufflefurnace/ProgramEditActivity.java
@@ -1,20 +1,32 @@
 package com.example.android.mufflefurnace;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.app.NavUtils;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.example.android.mufflefurnace.Data.ProgramContract;
+
 import static com.example.android.mufflefurnace.R.id.action_edit_program_name;
 
-public class ProgramEditActivity extends AppCompatActivity {
+public class ProgramEditActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
     /** Content URI for the existing program (null if it's a new pet) */
+
+
+    private static final int EXISTING_PROGRAM_ID_LOADER = 1;
+
     private Uri mCurrentProgramUri;
+    private String mCurrentProgramName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +50,7 @@ public class ProgramEditActivity extends AppCompatActivity {
             }
         });
 
+        getSupportLoaderManager().initLoader(EXISTING_PROGRAM_ID_LOADER, null, this);
     }
 
     @Override
@@ -45,8 +58,6 @@ public class ProgramEditActivity extends AppCompatActivity {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
         // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_program_edit, menu);
-
-
         return true;
     }
 
@@ -69,8 +80,52 @@ public class ProgramEditActivity extends AppCompatActivity {
             case R.id.action_delete:
                 // Do nothing for now
                 return true;
+            case android.R.id.home:
+                // Navigate back to parent activity (ProgramViewActivity)
+                Intent intent1 = new Intent(ProgramEditActivity.this, ProgramViewActivity.class);
+                intent1.setData(mCurrentProgramUri);
+                //startActivity(intent1);
+                NavUtils.navigateUpTo(this,intent1);
+                return true;
+
         }
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (mCurrentProgramUri == null){
+            return null;
+        }
+        String [] projection = {
+                ProgramContract.ProgramEntry.COLUMN_PROGRAM_NAME
+        };
+
+        return new CursorLoader(this,
+                mCurrentProgramUri,
+                projection,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Bail early if the cursor is null or there is less than 1 row in the cursor
+        if (cursor == null || cursor.getCount() <1){
+            return;
+        }
+        if (cursor.moveToFirst()){
+            int  currentProgramIDIndex = cursor.getColumnIndex(ProgramContract.ProgramEntry.COLUMN_PROGRAM_NAME);
+            mCurrentProgramName = cursor.getString(currentProgramIDIndex);
+
+            setTitle(mCurrentProgramName);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
--- app/src/main/java/com/example/android/mufflefurnace/ProgramViewActivity.java
+++ app/src/main/java/com/example/android/mufflefurnace/ProgramViewActivity.java
@@ -1,16 +1,24 @@
 package com.example.android.mufflefurnace;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ProgramViewActivity extends AppCompatActivity {
+import com.example.android.mufflefurnace.Data.ProgramContract;
 
+public class ProgramViewActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+    private static final int EXISTING_PROGRAM_ID_LOADER = 1;
 
     private Uri mCurrentProgramUri;
+    private String mCurrentProgramName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,8 @@ public class ProgramViewActivity extends AppCompatActivity {
         //in order to figure out if we're creating a new pet or editing existing one.
         Intent intent = getIntent();
         mCurrentProgramUri = intent.getData();
+
+        getSupportLoaderManager().initLoader(EXISTING_PROGRAM_ID_LOADER, null, this);
     }
 
     @Override
@@ -55,4 +65,40 @@ public class ProgramViewActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (mCurrentProgramUri == null){
+            return null;
+        }
+        String [] projection = {
+                ProgramContract.ProgramEntry.COLUMN_PROGRAM_NAME
+        };
+
+        return new CursorLoader(this,
+                mCurrentProgramUri,
+                projection,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Bail early if the cursor is null or there is less than 1 row in the cursor
+        if (cursor == null || cursor.getCount() <1){
+            return;
+        }
+        if (cursor.moveToFirst()){
+            int  currentProgramIDIndex = cursor.getColumnIndex(ProgramContract.ProgramEntry.COLUMN_PROGRAM_NAME);
+            mCurrentProgramName = cursor.getString(currentProgramIDIndex);
+
+            setTitle(mCurrentProgramName);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
