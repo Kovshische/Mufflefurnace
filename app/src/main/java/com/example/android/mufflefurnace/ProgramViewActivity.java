03b6e6af78b9c2a63605aafc5fef715395140773
Kovshische <Kovshische@gmail.com>
2017-08-29 11:51:19 +0300

    ProgramView - the point are displayed

--- app/src/main/java/com/example/android/mufflefurnace/ProgramViewActivity.java
+++ app/src/main/java/com/example/android/mufflefurnace/ProgramViewActivity.java
@@ -10,20 +10,32 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import com.example.android.mufflefurnace.Data.ProgramContract;
 
 public class ProgramViewActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
     private static final int EXISTING_PROGRAM_ID_LOADER = 1;
+    private static final int POINTS_LOADER = 2;
 
     private Uri mCurrentProgramUri;
     private String mCurrentProgramName;
+    private int mCurrentProgramId;
+
+    PointCursorAdapter mPointCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_program_view);
+
+
+        //find the ListView which will be populated with the program data
+        ListView pointListView = (ListView) findViewById(R.id.list_view_points);
+
+        mPointCursorAdapter = new PointCursorAdapter(this, null);
+        pointListView.setAdapter(mPointCursorAdapter);
 
         //Examine the intent that was used to launch this activity
         //in order to figure out if we're creating a new pet or editing existing one.
@@ -31,6 +43,10 @@ public class ProgramViewActivity extends AppCompatActivity implements LoaderMana
         mCurrentProgramUri = intent.getData();
 
         getSupportLoaderManager().initLoader(EXISTING_PROGRAM_ID_LOADER, null, this);
+    }
+
+    private void initPointLoader() {
+        getSupportLoaderManager().initLoader(POINTS_LOADER, null, this);
     }
 
     @Override
@@ -67,38 +83,80 @@ public class ProgramViewActivity extends AppCompatActivity implements LoaderMana
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        if (mCurrentProgramUri == null){
-            return null;
-        }
-        String [] projection = {
-                ProgramContract.ProgramEntry.COLUMN_PROGRAM_NAME
-        };
+        if (id == EXISTING_PROGRAM_ID_LOADER) {
+            if (mCurrentProgramUri == null) {
+                return null;
+            }
+            String[] projection = {
+                    ProgramContract.ProgramEntry._ID,
+                    ProgramContract.ProgramEntry.COLUMN_PROGRAM_NAME
+            };
 
-        return new CursorLoader(this,
-                mCurrentProgramUri,
-                projection,
-                null,
-                null,
-                null
-        );
+            return new CursorLoader(this,
+                    mCurrentProgramUri,
+                    projection,
+                    null,
+                    null,
+                    null
+            );
+
+        }
+
+        if (id == POINTS_LOADER)
+        {
+            String[] projectionForPoint = {
+                    ProgramContract.ProgramEntry.COLUMN_PROGRAM_ID,
+                    ProgramContract.ProgramEntry._ID,
+                    ProgramContract.ProgramEntry.COLUMN_TIME,
+                    ProgramContract.ProgramEntry.COLUMN_TEMPERATURE
+            };
+
+            String mCurrentProgramIdString = Integer.toString(mCurrentProgramId);
+
+            // Select Where ProgramId = currentProgramID
+            String select = "(" + ProgramContract.ProgramEntry.COLUMN_PROGRAM_ID + "=" + mCurrentProgramIdString + "  )";
+
+
+            return new CursorLoader(
+                    this,
+                    ProgramContract.ProgramEntry.CONTENT_URI_POINTS,
+                    projectionForPoint,
+                    select,
+                    null,
+                    ProgramContract.ProgramEntry.COLUMN_TIME
+            );
+        }
+        else return null;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        // Bail early if the cursor is null or there is less than 1 row in the cursor
-        if (cursor == null || cursor.getCount() <1){
-            return;
-        }
-        if (cursor.moveToFirst()){
-            int  currentProgramIDIndex = cursor.getColumnIndex(ProgramContract.ProgramEntry.COLUMN_PROGRAM_NAME);
-            mCurrentProgramName = cursor.getString(currentProgramIDIndex);
+        switch (loader.getId()) {
+            case EXISTING_PROGRAM_ID_LOADER:
+                // Bail early if the cursor is null or there is less than 1 row in the cursor
+                if (cursor == null || cursor.getCount() < 1) {
+                    return;
+                }
+                if (cursor.moveToFirst()) {
+                    int currentProgramIDIndex = cursor.getColumnIndex(ProgramContract.ProgramEntry.COLUMN_PROGRAM_NAME);
+                    int currentProgramIdIndex = cursor.getColumnIndexOrThrow(ProgramContract.ProgramEntry._ID);
 
-            setTitle(mCurrentProgramName);
+                    mCurrentProgramName = cursor.getString(currentProgramIDIndex);
+                    mCurrentProgramId = cursor.getInt(currentProgramIdIndex);
+
+                    setTitle(mCurrentProgramName);
+                }
+                initPointLoader();
+                break;
+
+
+            case POINTS_LOADER:
+                mPointCursorAdapter.swapCursor(cursor);
         }
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-
+        mPointCursorAdapter.swapCursor(null);
     }
 }
